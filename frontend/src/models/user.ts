03f1530520c7badf4b201b0588244df1ea714215
Samuel Lin <samuellin@yahoo.com>
2025-09-13 02:22:15 -0400

    change login collection

--- frontend/src/models/user.ts
+++ frontend/src/models/user.ts
@@ -9,7 +9,10 @@ interface IUserDocument extends IUser, Document {}
 const UserSchema = new Schema<IUser>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-});
+  },
+  {
+    collection: "users",
+  });
 
-const User: Model<IUserDocument>  = mongoose.models.User || mongoose.model<IUser>('User', UserSchema, "users");
+const User: Model<IUserDocument>  = mongoose.models.User || mongoose.model<IUser>('User', UserSchema);
 export default User;
